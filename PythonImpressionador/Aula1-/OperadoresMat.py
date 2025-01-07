636d97d083709df42cc95ecfec410eefb85627c4
Guilherme <guilhermersouza34@gmail.com>
2025-01-06 21:57:12 -0300

    Desafio100DaysOfCode-OperadoresMatematicos

---
+++ PythonImpressionador/Aula1-/OperadoresMat.py
@@ -0,0 +1,5 @@
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+num3 = float(input("Digite o Terceiro numero: "))
+media = (num1 + num2 + num3) / 3
+print(f"A media dos numeros {num1}, {num2} e {num3} é {media:.2f}")
