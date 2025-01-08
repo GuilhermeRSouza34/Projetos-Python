d7b0bcdd218121bb27aef550810fd8fe850eaf60
Guilherme <guilhermersouza34@gmail.com>
2025-01-07 21:02:03 -0300

    Desafio100DaysOfCode-InversãoDeValores

---
+++ PythonImpressionador/Aula2-Variables/InvValores.py
@@ -0,0 +1,7 @@
+valor1 = input("Digite o primeiro valor: ")
+valor2 = input("Digite o segundo valor: ")
+
+# Trocando os valores
+valor1, valor2 = valor2, valor1
+
+print(f"Após a troca, o primeiro valor é {valor1} e o segundo valor é {valor2}.")
