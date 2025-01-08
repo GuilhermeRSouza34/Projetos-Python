ba7b8365adf76d5e49fc743943078e8f7546e57f
Guilherme <guilhermersouza34@gmail.com>
2025-01-07 21:04:27 -0300

    Desafio100DaysOfCode-VelocidadeMedia

---
+++ PythonImpressionador/Aula2-Variables/VeloMedia.py
@@ -0,0 +1,4 @@
+distancia = float(input("Digite a distancia percorrida (em km): "))
+tempo = float(input("Digite o tempo gasto (em horas): "))
+velocidade_media = distancia / tempo
+print(f"A velocidade média foi de {velocidade_media:.2f} km/h.")
