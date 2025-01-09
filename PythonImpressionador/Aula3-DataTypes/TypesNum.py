6705e07853868933310c006e94619a7af8125653
Guilherme <guilhermersouza34@gmail.com>
2025-01-08 21:25:48 -0300

    Desafio100DaysOfCode-TiposDeNumeros

---
+++ PythonImpressionador/Aula3-DataTypes/TypesNum.py
@@ -0,0 +1,11 @@
+#Exemplo de int
+numero_inteiro = 10
+print(f"Numero inteiro: {numero_inteiro} tipo: {type(numero_inteiro)}")
+
+#Exemplo de float
+numero_float = 10.5
+print(f"Numero Decimal: {numero_float} tipo: {type(numero_float)}")
+
+#Exemplo de numero complexo
+numero_complexo = 2 + 3j
+print(f"Numero Complexo: {numero_complexo} tipo: {type(numero_complexo)}")
