2f2759a66dbea32eb8850c13b2c8a4b27b7e52ea
Guilherme <guilhermersouza34@gmail.com>
2025-01-09 19:32:56 -0300

    Desafio100DaysOfCode-Exemplo2-condicionais

--- PythonImpressionador/Aula4-Condicionais/Exemplos.py
+++ PythonImpressionador/Aula4-Condicionais/Exemplos.py
@@ -3,3 +3,9 @@ idade = 20
 if idade >= 18:
     print("Você é maior de idade")
     
+# Exemplo 2
+idade2 = 15
+if idade2 >= 18:
+    print("Você é maior de idade")
+else:
+    print("Você não é maior de idade")
