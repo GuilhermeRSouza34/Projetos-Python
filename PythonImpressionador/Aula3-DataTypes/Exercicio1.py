f14a3daecbd472aae2ff294641542bfd98dd75a3
Guilherme <guilhermersouza34@gmail.com>
2025-01-08 21:30:58 -0300

    Desafio100DaysOfCode-Exercicio-TiposDeNumeros

---
+++ PythonImpressionador/Aula3-DataTypes/Exercicio1.py
@@ -0,0 +1,17 @@
+# Exercício 1: Manipulando Números
+# 1 - Crie uma variável para armazenar sua idade (como inteiro).
+# 2 - Crie outra para armazenar sua altura (como decimal).
+# 3 - Some as duas variáveis e veja o resultado.
+# 4 - Multiplique o resultado por um número complexo (exemplo: 2 + 1j).
+
+idade = 20
+altura = 1.80
+
+# Soma
+soma = idade + altura
+print(f"Soma: {soma}")
+
+# Multiplicação com número complexo
+numero_complexo = 20 + 30j
+resultado = soma * numero_complexo
+print(f"Resultado final: {resultado}")
