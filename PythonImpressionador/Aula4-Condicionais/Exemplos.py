03266e619f85086a01408cd8acf22b7b038abdc9
Guilherme <guilhermersouza34@gmail.com>
2025-01-09 19:42:35 -0300

    Desafio100DaysOfCode-Exemplo5-condicionais

--- PythonImpressionador/Aula4-Condicionais/Exemplos.py
+++ PythonImpressionador/Aula4-Condicionais/Exemplos.py
@@ -3,12 +3,19 @@ idade = 20
 if idade >= 18:
     print("Você é maior de idade")
     
+    
+    
+    
+    
 # Exemplo 2
 idade2 = 15
 if idade2 >= 18:
     print("Você é maior de idade")
 else:
     print("Você não é maior de idade")
+    
+    
+    
     
 # Exemplo 3 utilizando elif
 idade3 = 20
@@ -19,6 +26,9 @@ elif idade3 < 18:
 else:
     print("Você é um adulto")
     
+    
+    
+    
 # Exemplo 4 Condiçoes compostas utilizando o AND
 idade4 = 25
 tem_habilitacao = True
@@ -27,6 +37,9 @@ if idade4 >= 18 and tem_habilitacao:
     print("Você pode dirigir")
 else:
     print("Você não pode dirigir")
+    
+    
+    
     
 # Exemplo 4.1 Condiçoes compostas utilizando o OR
 idade5 = 25
@@ -37,6 +50,9 @@ if idade5 >= 18 or tem_habilitacao1:
 else:
     print("Você não pode dirigir")
     
+    
+    
+    
 # Exemplo 4.2 Condiçoes compostas utilizando o NOT
 idade6 = 25
 tem_habilitacao2 = True
@@ -45,3 +61,10 @@ if not tem_habilitacao2:
     print("Você precisa tirar a habilitacao")
 else:
     print("Você pode dirigir")
+    
+    
+    
+# Exemplo 5 condicionais em um unico comando
+idade7 = 18
+status = "Adulto" if idade7 >= 18 else "Menor de idade"
+print(f"Você é {status}")
