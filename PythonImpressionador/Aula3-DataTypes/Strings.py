842dcc0e92397b32a6c23717b82eb16f066e0279
Guilherme <guilhermersouza34@gmail.com>
2025-01-08 21:38:51 -0300

    Desafio100DaysOfCode-Strings

---
+++ PythonImpressionador/Aula3-DataTypes/Strings.py
@@ -0,0 +1,11 @@
+# Exemplo de String
+
+nome = "Guilherme"
+frase = "Vai se tornar um iron man"
+
+print(nome.upper()) # para retornar o nome em maiúsculo
+# print(nome.lower()) ||para retornar o nome em minúsculo||
+# print(frase.capitalize()) ||para retornar a frase com a primeira letra em maiúsculo||
+# print(frase.title()) ||para retornar a frase com a primeira letra de cada palavra em maiúsculo||
+print(frase.split()) # Divide em palavras
+print(f"Nome: {nome}, tipo: {type(nome)}")
