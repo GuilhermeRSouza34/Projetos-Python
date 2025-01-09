e6e4b79c14f789c65eae63524556743befdf5f4c
Guilherme <guilhermersouza34@gmail.com>
2025-01-09 19:52:20 -0300

    Desafio100DaysOfCode-Exercicio3

---
+++ PythonImpressionador/Aula4-Condicionais/AproRepro.py
@@ -0,0 +1,11 @@
+# Exercício 3: Aprovado ou Reprovado
+# Crie um programa que leia a nota de um aluno e informe se ele foi aprovado ou reprovado. O aluno será aprovado se a nota for maior ou igual a 7. Se a nota for menor que 7, o aluno será reprovado.
+
+nota = input("Digite a nota do aluno: ")
+if float(nota) >= 7:
+    print("Aprovado")
+elif float(nota) < 7 or float(nota) > 3:
+    print("Recuperação")
+else:
+    print("Reprovado")
+    
