29302b4d1cca499c3fdbdb1308820bf174933db6
Guilherme <guilhermersouza34@gmail.com>
2025-01-08 21:44:53 -0300

    Desafio100DaysOfCode-Listas

---
+++ PythonImpressionador/Aula3-DataTypes/List.py
@@ -0,0 +1,7 @@
+# Lista de numeros
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8]
+numeros.append(9) # para adicionar um número
+numeros.remove(1) # para remover o numero 1
+print(f"Lista de números: {numeros}")
+
