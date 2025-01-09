14de36b580a6f7f2960146bd10abad62cc8ec6e5
Guilherme <guilhermersouza34@gmail.com>
2025-01-09 19:39:27 -0300

    Desafio100DaysOfCode-Exemplo4-condicionais

--- PythonImpressionador/Aula4-Condicionais/Exemplos.py
+++ PythonImpressionador/Aula4-Condicionais/Exemplos.py
@@ -19,3 +19,29 @@ elif idade3 < 18:
 else:
     print("Você é um adulto")
     
+# Exemplo 4 Condiçoes compostas utilizando o AND
+idade4 = 25
+tem_habilitacao = True
+
+if idade4 >= 18 and tem_habilitacao:
+    print("Você pode dirigir")
+else:
+    print("Você não pode dirigir")
+    
+# Exemplo 4.1 Condiçoes compostas utilizando o OR
+idade5 = 25
+tem_habilitacao1 = True
+
+if idade5 >= 18 or tem_habilitacao1:
+    print("Você pode dirigir")
+else:
+    print("Você não pode dirigir")
+    
+# Exemplo 4.2 Condiçoes compostas utilizando o NOT
+idade6 = 25
+tem_habilitacao2 = True
+
+if not tem_habilitacao2:
+    print("Você precisa tirar a habilitacao")
+else:
+    print("Você pode dirigir")
