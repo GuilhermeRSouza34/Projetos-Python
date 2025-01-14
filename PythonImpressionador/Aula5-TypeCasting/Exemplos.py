553c822a3624d7880d3edc7fbc9f51b007317f11
Guilherme <guilhermersouza34@gmail.com>
2025-01-13 21:49:23 -0300

    Desafio100DaysOfCode-Ex2: Conversão Explicita

--- PythonImpressionador/Aula5-TypeCasting/Exemplos.py
+++ PythonImpressionador/Aula5-TypeCasting/Exemplos.py
@@ -4,3 +4,7 @@ decimal = 2.5
 soma = inteiro + decimal
 print(soma) # Resposta vai ser: 12.5 (float)
 
+# EXEMPLO 2 - CONVERSÃO EXPLÍCITA
+texto = "50"
+numero = int(texto)
+print(numero) # Resposta vai ser: 50 (int)
