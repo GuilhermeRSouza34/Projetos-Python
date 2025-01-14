e6e5e71f7ae950904c12d03ae8e38c6e4bd76dd6
Guilherme <guilhermersouza34@gmail.com>
2025-01-13 21:51:46 -0300

    Desafio100DaysOfCode-Ex3-MisturaDeTipos

--- PythonImpressionador/Aula5-TypeCasting/Exemplos.py
+++ PythonImpressionador/Aula5-TypeCasting/Exemplos.py
@@ -8,3 +8,9 @@ print(soma) # Resposta vai ser: 12.5 (float)
 texto = "50"
 numero = int(texto)
 print(numero) # Resposta vai ser: 50 (int)
+
+# EXEMPLO 3 - MISTURA DE TIPOS
+inteiro2 = 10
+float_para_int = int(4.9)
+resultado2 = inteiro2 + float_para_int
+print(resultado2) # Resposta vai ser: 14 (int)
