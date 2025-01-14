80f125b2b2778fcd59c6aec494291f462bf9f4f5
Guilherme <guilhermersouza34@gmail.com>
2025-01-13 22:03:01 -0300

    Desafio100DaysOfCode-Exercicio4-CalculadoraDeArea

---
+++ PythonImpressionador/Aula5-TypeCasting/Exercicio4.py
@@ -0,0 +1,9 @@
+largura = input("Digite a largura do retangulo: ")
+altura = input("Digite a altura do retangulo: ")
+
+largura = float(largura)
+altura = float(altura)
+
+area = largura * altura
+
+print(f"A área do retângulo é {area} m².")
