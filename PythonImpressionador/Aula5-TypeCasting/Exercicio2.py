ddc22b85254630e20e2855b41369056e6252d8d3
Guilherme <guilhermersouza34@gmail.com>
2025-01-13 21:59:17 -0300

    Desafio100DaysOfCode-Exercicio2-ConversaoEntECalc

---
+++ PythonImpressionador/Aula5-TypeCasting/Exercicio2.py
@@ -0,0 +1,12 @@
+# Solicita que o usurio digite dois numeros
+numero1 = input("Digite o primeiro numero: ")
+numero2 = input("Digite o segundo numero: ")
+
+# Fução para converter os numeros para float
+numero1 = float(numero1)
+numero2 = float(numero2)
+
+# Calcula a media
+media = (numero1 + numero2) / 2
+
+print(f"A média dos números é: {media}")
